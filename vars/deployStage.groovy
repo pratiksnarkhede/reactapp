e58c997caa8aa7a83b228ba02e75ff019fdad5e5
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2023-06-15 15:49:12 +0530

    Update deployStage.groovy

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -3,7 +3,7 @@ def call() {
         steps {
             script {
                 def dockerRun = "sudo docker run -p 8080:80 -d --name react-app pratik0078/nodeapp:$BUILD_NUMBER"
-                sshagent(['docker-hub-pratik']) {
+                sshagent(['docker-server']) {
                     sh "ssh -o StrictHostKeyChecking=no azureuser@20.102.62.140 ${dockerRun}"
                 }
             }
