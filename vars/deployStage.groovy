e146bd51085962126be307046563c05d2a8a1c39
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2023-06-15 15:17:29 +0530

    Create deployStage.groovy

---
+++ vars/deployStage.groovy
@@ -0,0 +1,12 @@
+def call() {
+    stage('Run Container on Dev Server') {
+        steps {
+            script {
+                def dockerRun = "sudo docker run -p 8080:80 -d --name react-app pratik0078/nodeapp:$BUILD_NUMBER"
+                sshagent(['docker-server']) {
+                    sh "ssh -o StrictHostKeyChecking=no azureuser@20.102.62.140 ${dockerRun}"
+                }
+            }
+        }
+    }
+}
