0fded8fd7f351e796278732a27290e8802cf0b77
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2023-06-15 15:15:03 +0530

    Update sshAgent.groovy

--- vars/sshAgent.groovy
+++ vars/sshAgent.groovy
@@ -1,6 +1,3 @@
 def call(){
    sh "ssh -o StrictHostKeyChecking=no azureuser@20.102.62.140 ${dockerRun}"
 }
-def dockerRun(){
- "sudo docker run -p 8080:80 -d --name react-app pratik0078/nodeapp:$BUILD_NUMBER"
-}
